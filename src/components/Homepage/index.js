05fbeb0fd9d0520d3e7e15ce99e2b2a1f2aea86b
hugosmails94 <hugosmails94@gmail.com>
2019-09-04 17:09:05 +0200

    search_zone

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ListVideo from './ListVideo';
 import Tabs from '../Footer/Tabs';
+import Search from '../Header/Search'
 
 class Homepage extends React.PureComponent {
 	render() {
 		return (
 			<React.Fragment>
+				<Search/>
 				<Tabs/>
 				<ListVideo/>
 			</React.Fragment>
