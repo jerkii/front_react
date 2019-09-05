fd4057681a27bfcf19dec6d65de5f5451540b3b3
hugosmails94 <hugosmails94@gmail.com>
2019-09-05 15:01:54 +0200

    external route file and define home as root /

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import Homepage from './components/Homepage';
 
@@ -6,3 +7,21 @@ const Routes = {
 };
 
 export default Routes;
+=======
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Homepage from './components/Homepage';
+import Videos from './components/Homepage/ListVideo/video';
+
+export default function Routes () {
+	return (
+		<Router>
+			<div>
+				<Route exact path="/" component={Homepage}/>
+				<Route path="/video" component={Videos}/>
+			</div>
+		</Router>
+	)
+}
+>>>>>>> 482a91c... external route file and define home as root /
