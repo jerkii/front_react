eb830ecff474da41446a885de41c1451ce41d0fc
hugosmails94 <hugosmails94@gmail.com>
2019-09-09 15:16:12 +0200

    fixup! fixup! fixup! Merge branch 'master' into react_router_hook

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Homepage from './components/Homepage';
 
@@ -7,21 +6,3 @@ const Routes = {
 };
 
 export default Routes;
-=======
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-
-import Homepage from './components/Homepage';
-import Videos from './components/Homepage/ListVideo/video';
-
-export default function Routes () {
-	return (
-		<Router>
-			<div>
-				<Route exact path="/" component={Homepage}/>
-				<Route path="/video" component={Videos}/>
-			</div>
-		</Router>
-	)
-}
->>>>>>> 482a91c... external route file and define home as root /
