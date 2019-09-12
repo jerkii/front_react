e23f2f831a5f8271142b7ea5efe7e93c30cfbb91
hugosmails94 <hugosmails94@gmail.com>
2019-09-12 09:30:11 +0200

    fixup! comments zone

--- src/components/VideoPage/Comments/index.js
+++ src/components/VideoPage/Comments/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
 const toggleCommentCssClass = (index) => {
-    return index%2 === 0 ? 'left' : 'right';
+	return index % 2 === 0 ? 'left' : 'right';
 }
 
 const Comments = ({ comments }) => {
-    
 	return (
 		<>
 			<div className="comments">
-                <div className="comments-line"></div>
-                {comments.map((comment, index) =>
-                    <p key={comments.id} className={`comments-text__${toggleCommentCssClass(index)}`}>{comment.text}</p>
-                )}
+				<div className="comments-line"></div>
+				{comments.map((comment, index) =>
+					<p key={comments.id} className={`comments-text__${toggleCommentCssClass(index)}`}>{comment.text}</p>
+				)}
 			</div>
 		</>
 	);
